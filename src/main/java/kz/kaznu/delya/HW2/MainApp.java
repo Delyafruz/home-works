7841293a806737f6d8297975b6e4a2c133d9ad39
Delya <ketsu715@gmail.com>
2025-02-05 20:49:16 +0500

    update HW2

--- src/main/java/kz/kaznu/delya/HW2/MainApp.java
+++ src/main/java/kz/kaznu/delya/HW2/MainApp.java
@@ -4,10 +4,17 @@ import java.util.Arrays;
 
 public class MainApp {
     public static void main(String[] args) {
+        
     }
-
+    public static void sameSentences() {
+        int[] arr= new int[6];
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("Hi everyone!");
+        }
+        System.out.println(Arrays.toString(arr)); //просто чтоб проверить наш массив
+    }
     public static void sumArr() {
-        //задание №1
+        //задание №2
         int[] arr = {3, 6, 7, 9, 2, 4};
         int sum = 0;
         for (int i = 0; i < arr.length; i++) {
@@ -20,7 +27,7 @@ public class MainApp {
     }
 
     public static void sameNum1() {
-        //задание №2 а)
+        //задание №3 а)
         int[] arr = new int[7];
         for (int i = 0; i < arr.length; i++) {
             arr[i] = 3;
@@ -29,7 +36,7 @@ public class MainApp {
     }
 
     public static void sameNum2() {
-        //задание №2 б)
+        //задание №3 б)
         int[] arr = {7, 3, 8, 4, 3};
         for (int i = 0; i < arr.length; i++) {
             arr[i] = 4;
@@ -38,7 +45,7 @@ public class MainApp {
     }
 
     public static void plusNum() {
-        //задание №3
+        //задание №4
         int[] arr = {3, 6, 2, 2, 3};
         for (int i = 0; i < arr.length; i++) {
             arr[i] += 3;
@@ -47,7 +54,7 @@ public class MainApp {
     }
 
     public static void sum1AndSum2() {
-        //задание №4
+        //задание №5
         int[] arr = {3, 2, -1, 3, 2, 4};
         int sum1 = 0;
         int sum2 = 0;
