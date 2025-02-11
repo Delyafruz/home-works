47dbbf9eee643fc0ffcc775bb3b09dddc4fbbf85
Delya <ketsu715@gmail.com>
2025-02-11 10:29:14 +0500

    Update MainApp.java

--- src/main/java/kz/kaznu/delya/HW2/MainApp.java
+++ src/main/java/kz/kaznu/delya/HW2/MainApp.java
@@ -17,7 +17,7 @@ public class MainApp {
         plusNum(3, array2);
         System.out.println(Arrays.toString(array2));
         ////////////
-        int[] arr2 = {2, 5, 3, 2, 6, 4, 2};
+        int[] arr2 = {2, 1, 1, 2};
         sum1AndSum2(arr2);
     }
 
@@ -64,18 +64,19 @@ public class MainApp {
         for (int i = arr.length / 2; i < arr.length; i++) {
             sum2 += arr[i];
         }
-        if (arr.length%2!=0){
+        if (arr.length % 2 != 0) {
             System.out.println(sum1);
-        }else{
+        } else {
             System.out.println(sum2);
         }
         if (sum1 > sum2) {
             System.out.println("Первая часть массива больше:");
             System.out.println(sum1);
-        } else {
+        } else if (sum1 < sum2) {
             System.out.println("Вторая часть массива больше:");
             System.out.println(sum2);
+        } else {
+            System.out.println("Обе части массива равны.");
         }
-        System.out.println(Arrays.toString(arr));
     }
 }
