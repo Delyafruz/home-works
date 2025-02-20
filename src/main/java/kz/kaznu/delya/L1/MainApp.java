63478619ec948ff93b3bb2dcafc6653fb5cc75f7
Delya <ketsu715@gmail.com>
2025-02-20 21:05:12 +0500

    HW6

--- src/main/java/kz/kaznu/delya/L1/MainApp.java
+++ src/main/java/kz/kaznu/delya/L1/MainApp.java
@@ -1,4 +1,32 @@
 package kz.kaznu.delya.L1;
 
+import kz.kaznu.delya.HW6.Cat;
+import kz.kaznu.delya.HW6.Plate;
+
 public class MainApp {
+    public static void main(String[] args) {
+        Plate plate=new Plate(50);
+
+
+        Cat[] cats={
+                new Cat("Барсик", 5),
+                new Cat("Мурзик", 7),
+                new Cat("Том", 8),
+                new Cat("Гарфилд", 10)
+        };
+        for (int i = 0; i < cats.length; i++) {
+            cats[i].eat(plate);
+        }
+        for (int i = 0; i < cats.length; i++) {
+            cats[i].info();
+        }
+        plate.addFood(10);
+
+        for (int i = 0; i < cats.length; i++) {
+            cats[i].eat(plate);
+        }
+        for (int i = 0; i < cats.length; i++) {
+            cats[i].info();
+        }
+    }
 }
